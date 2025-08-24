1067e9cc85883d6ca22d09e2779e8d0c9a28f3ee
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-08-23 20:48:28 -0400

    fix: Types on class variables from copying them over

--- android/src/main/java/com/cyanchill/missingcore/metadataretriever/models/MetadataItem.kt
+++ android/src/main/java/com/cyanchill/missingcore/metadataretriever/models/MetadataItem.kt
@@ -2,44 +2,44 @@ package com.cyanchill.missingcore.metadataretriever.model
 
 interface MetadataItem {
   /* List of fields available on `Format`. */
-  var bitrate: number?
-  var channelCount: number?
-  var codecs: string?
-  var sampleMimeType: string?
-  var sampleRate: number? // in `Hz`
+  var bitrate: Int?
+  var channelCount: Int?
+  var codecs: String?
+  var sampleMimeType: String?
+  var sampleRate: Int? // in `Hz`
   /* List of fields available on `MediaMetadata`. */
-  var albumArtist: string?
-  var albumTitle: string?
-  var artist: string?
-  var artworkData: string?
-  var artworkDataType: string?
-  var artworkUri: string?
-  var compilation: string?
-  var composer: string?
-  var conductor: string?
-  var description: string?
-  var discNumber: number?
-  var displayTitle: string?
+  var albumArtist: String?
+  var albumTitle: String?
+  var artist: String?
+  var artworkData: String?
+  var artworkDataType: String?
+  var artworkUri: String?
+  var compilation: String?
+  var composer: String?
+  var conductor: String?
+  var description: String?
+  var discNumber: Int?
+  var displayTitle: String?
   // var extras: Any?
-  var genre: string?
-  var isBrowsable: boolean?
-  var isPlayable: boolean?
-  var mediaType: string?
-  var overallRating: number?
-  var recordingDay: number?
-  var recordingMonth: number?
-  var recordingYear: number?
-  var releaseDay: number?
-  var releaseMonth: number?
-  var releaseYear: number?
-  var station: string?
-  var subtitle: string?
-  var title: string?
-  var totalDiscCount: number?
-  var totalTrackCount: number?
-  var trackNumber: number?
-  var userRating: number?
-  var writer: string?
+  var genre: String?
+  var isBrowsable: Boolean?
+  var isPlayable: Boolean?
+  var mediaType: String?
+  var overallRating: Double?
+  var recordingDay: Int?
+  var recordingMonth: Int?
+  var recordingYear: Int?
+  var releaseDay: Int?
+  var releaseMonth: Int?
+  var releaseYear: Int?
+  var station: String?
+  var subtitle: String?
+  var title: String?
+  var totalDiscCount: Int?
+  var totalTrackCount: Int?
+  var trackNumber: Int?
+  var userRating: Double?
+  var writer: String?
   /* List of custom fields derived from other fields. */
-  var year: number?
+  var year: Int?
 }
