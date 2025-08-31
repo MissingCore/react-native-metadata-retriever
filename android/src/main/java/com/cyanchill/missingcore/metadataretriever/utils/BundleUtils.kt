5282c78e6707b3b940f4ef4ac3260134b8d0255a
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-08-31 19:04:42 -0400

    fix: Support `null` values w/ `BundleUtils.putDoubleIfExists`

--- android/src/main/java/com/cyanchill/missingcore/metadataretriever/utils/BundleUtils.kt
+++ android/src/main/java/com/cyanchill/missingcore/metadataretriever/utils/BundleUtils.kt
@@ -3,7 +3,15 @@ package com.cyanchill.missingcore.metadataretriever.utils
 import android.os.Bundle
 
 object BundleUtils {
+  fun getDoubleOrNull(key: String, data: Bundle): Double? {
+    return data[key] as? Double
+  }
+
   fun putDoubleIfExists(key: String, data: Bundle, store: Bundle) {
-    if (data.containsKey(key)) store.putDouble(key, data.getDouble(key))
+    if (data.containsKey(key)) {
+      val value = getDoubleOrNull(key, data)
+      if (value != null) store.putDouble(key, value)
+      else store.remove(key)
+    }
   }
 }
