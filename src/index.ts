948a1834133656016d3aa4117eb3321b8ceef2c9
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-08-31 20:04:24 -0400

    chore: Explicitly mark functions as async

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import type {
 import { MediaMetadataPublicFields, MetadataPresets } from './constants';
 
 /** Returns the specified metadata of a media file from its uri. */
-export function getMetadata<TOptions extends MediaMetadataPublicFields>(
+export async function getMetadata<TOptions extends MediaMetadataPublicFields>(
   uri: string,
   options: TOptions
 ): Promise<MediaMetadataExcerpt<TOptions>> {
@@ -22,11 +22,11 @@ export function getMetadata<TOptions extends MediaMetadataPublicFields>(
  * Returns the artwork of the specified media file from its uri.
  * - Defaults to returning up to `5 MB` of data.
  */
-export function getArtwork(uri: string): Promise<string | null> {
+export async function getArtwork(uri: string): Promise<string | null> {
   return MetadataRetriever.getArtwork(uri);
 }
 
-export function updateConfigs(options: ConfigOptions): Promise<void> {
+export async function updateConfigs(options: ConfigOptions): Promise<void> {
   return MetadataRetriever.updateConfigs(options);
 }
 
