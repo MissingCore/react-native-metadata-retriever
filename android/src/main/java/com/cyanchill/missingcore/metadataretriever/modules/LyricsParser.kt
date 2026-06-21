decbd80d1194cda491eaa0be8f78ee62a91724bb
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-06-21 01:14:40 -0400

    fix: How we determine encoding if encoding byte is 1
    - We were previously checking the bytes representing "language", which isn't encoded.

--- android/src/main/java/com/cyanchill/missingcore/metadataretriever/modules/LyricsParser.kt
+++ android/src/main/java/com/cyanchill/missingcore/metadataretriever/modules/LyricsParser.kt
@@ -58,6 +58,12 @@ class LyricsParser(metadataList: List<Metadata>) {
     //  - Mp3Tag doesn't specify a Byte Order Mark if it's `1` (UTF-16), so we'll do a heuristic guess for what charset to use.
     //  - Ref: https://mutagen-specs.readthedocs.io/en/latest/id3/id3v2.4.0-structure.html#id3v2-frame-overview
     val encodingByte = byteArr[0].toString()
+    // `USLT` starts with 4 bytes of "junk" while `SYLT` starts with 6 bytes.
+    //  - https://mutagen-specs.readthedocs.io/en/latest/id3/id3v2.4.0-frames.html#unsynchronised-lyrics-text-transcription
+    //  - https://mutagen-specs.readthedocs.io/en/latest/id3/id3v2.4.0-frames.html#synchronised-lyrics-text
+    val headerSize = if (tagName == "USLT") 4 else 6
+    // Figure out the BOM used by checking the encoding on the "Content Descriptor" section.
+    byteArr = byteArr.drop(headerSize).toByteArray()
     val encodingCharSet = when (encodingByte) {
       "0" -> Charsets.ISO_8859_1
       "1" -> {
@@ -77,10 +83,6 @@ class LyricsParser(metadataList: List<Metadata>) {
     }
 
     val twoByteNull = encodingByte == "1" || encodingByte == "2"
-    // `USLT` starts with 4 bytes of "junk" while `SYLT` starts with 6 bytes.
-    //  - https://mutagen-specs.readthedocs.io/en/latest/id3/id3v2.4.0-frames.html#unsynchronised-lyrics-text-transcription
-    //  - https://mutagen-specs.readthedocs.io/en/latest/id3/id3v2.4.0-frames.html#synchronised-lyrics-text
-    byteArr = byteArr.drop(if (tagName == "USLT") 4 else 6).toByteArray()
     // We then have a "content descriptor", which ends with either 1 or 2 null bytes (based on the text encoding).
     var droppedBytes = 0
     for (i in 0 until byteArr.size - 1) {
